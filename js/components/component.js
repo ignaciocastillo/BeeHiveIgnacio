2ee30ca22721d24960ead6b94d738bd140da58f9
Ignacio Castillo <39069645+ignaciocastillo@users.noreply.github.com>
2021-04-26 17:06:52 -0600

    Revert "Commit"
    This reverts commit 97a748f71703e0462cc6b88bfa1f1ef0b6b72cf0.

--- js/components/component.js
+++ js/components/component.js
@@ -3,11 +3,9 @@ class Component {
         this.parent = parent;
         this.appManager = appManager;
         this.model = model;
-
-        this.container = div({
-            'className': className
-        }, this.parent, null)
+        this.container = div({ 'className': className }, this.parent, null);
     }
+
 
     hide() {
         this.container.hidden = true;
@@ -17,21 +15,5 @@ class Component {
     show() {
         this.container.hidden = false;
         this.container.classList.remove('hidden');
-        this.moveIn();
-    }
-
-    moveIn() {
-        gsap.to(this.container, {
-            duration: 1,
-            x: 0,
-        });
-    }
-
-    moveOut() {
-        gsap.to(this.container, {
-            duration: 1,
-            x: window.innerWidth,
-            onComplete: this.hide.bind(this)
-        });
     }
 }
--- js/managers/appManager.js
+++ js/managers/appManager.js
@@ -6,12 +6,5 @@ class AppManager {
         this.dataManager = new DataManager(this);
         this.uiManager = new UIManager(this);
         this.netManager.fetchBees();
-
-        this.appState_Bees = 1;
-        this.appState_PostList = 2;
-        this.appState_AlbumList = 3;
-        this.appState_TodoList = 4;
-
-        this.appState = this.appState_Bees;
     }
 }
